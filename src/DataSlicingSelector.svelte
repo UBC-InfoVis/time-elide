af7f37662a8f61bbfe570a36f217f60afba58393
Luce <lucy_liu@live.ca>
2021-02-22 22:02:27 -0500

    add manual slicer component

--- src/DataSlicingSelector.svelte
+++ src/DataSlicingSelector.svelte
@@ -1,18 +1,49 @@
 <script>
   // import "../node_modules/uikit/dist/css/uikit.min.css";
+  import ManualSlicer from "./ManualSlicer.svelte";
+
   export let disabled;
+  const MANUAL_SELECT = "manual select";
+  const DETECT_PERIODS = "detect periods";
+  const NONE_SELECTED = "none selected";
+  let slicerMode = NONE_SELECTED;
+
+  const handleManualSelectClick = () => {
+    console.log("clicked MANUAL SELECT");
+    slicerMode = MANUAL_SELECT;
+  };
+
+  const handleDetectClick = () => {
+    console.log("detect period automatically");
+    slicerMode = DETECT_PERIODS;
+  };
 </script>
 
 <div class:disabled>
   <p>Select slicing method</p>
-  <button class="uk-button uk-button-default uk-button-large option" {disabled}
-    >Option 1
-    <h3>Select periods manually</h3>
-  </button>
-  <button class="uk-button uk-button-default uk-button-large option" {disabled}
-    >Option 2
-    <h3>Detect periods automatically</h3>
-  </button>
+  {#if slicerMode === NONE_SELECTED}
+    <button
+      on:click={handleManualSelectClick}
+      class="uk-button uk-button-default uk-button-large option"
+      {disabled}
+      >Option 1
+      <h3>Select periods manually</h3>
+    </button>
+    <button
+      on:click={handleDetectClick}
+      class="uk-button uk-button-default uk-button-large option"
+      {disabled}
+      >Option 2
+      <h3>Detect periods automatically</h3>
+    </button>
+  {:else if slicerMode === MANUAL_SELECT}
+    <!-- Slicer component -->
+    <ManualSlicer />
+  {:else}
+    <div>
+      <p>Automatic slicer goes here</p>
+    </div>
+  {/if}
 </div>
 
 <style>
---
+++ src/ManualSlicer.svelte
@@ -0,0 +1,111 @@
+<script>
+  let data = []; // Will contain chosen time slice filters
+
+  function addRow() {
+    console.log(newRow);
+    data = [...data, newRow];
+    resetNewRow();
+  }
+
+  function deleteRow(rowToBeDeleted) {
+    data = data.filter((row) => row != rowToBeDeleted);
+  }
+
+  function resetNewRow() {
+    newRow = { day: 1, fromTime: "", toTime: "" };
+  }
+
+  let dayOptions = [
+    { id: "1", value: "Monday" },
+    { id: "2", value: "Tuesday" },
+    { id: "3", value: "Wednesday" },
+    { id: "4", value: "Thursday" },
+    { id: "5", value: "Friday" },
+    { id: "6", value: "Saturday" },
+    { id: "0", value: "Sunday" },
+  ];
+
+  let newRow = {};
+  resetNewRow();
+
+  // Disable add-button if input fields are empty
+  $: newRowValid = newRow.startTime && newRow.endTime;
+</script>
+
+<div>
+  <p>Select periods manually</p>
+  <table>
+    <tr>
+      <th>Day</th>
+      <th>From</th>
+      <th>To</th>
+    </tr>
+
+    {#each data as row}
+      <tr>
+        <td>
+          <select bind:value={row.day}>
+            {#each dayOptions as option}
+              <option value={option.id} selected={row.day === option.id}
+                >{option.value}</option
+              >
+            {/each}
+          </select>
+        </td>
+        <td
+          ><input
+            type="time"
+            step="300"
+            bind:value={row.startTime}
+            required
+          /></td
+        >
+        <td
+          ><input
+            type="time"
+            step="300"
+            bind:value={row.endTime}
+            required
+          /></td
+        >
+        <button on:click={() => deleteRow(row)}> X </button>
+      </tr>
+    {/each}
+
+    <tr class="new">
+      <td>
+        <select bind:value={newRow.day}>
+          {#each dayOptions as option}
+            <option value={option.id} selected={newRow.day === option.id}
+              >{option.value}</option
+            >
+          {/each}
+        </select>
+      </td>
+      <td
+        ><input
+          type="time"
+          step="300"
+          bind:value={newRow.startTime}
+          required
+        /></td
+      >
+      <td
+        ><input
+          type="time"
+          step="300"
+          bind:value={newRow.endTime}
+          required
+        /></td
+      >
+      <button on:click={addRow} disabled={!newRowValid}>Add</button>
+    </tr>
+  </table>
+</div>
+
+<style>
+  /* table {
+    table-layout: fixed;
+    width: 100%;
+  } */
+</style>
